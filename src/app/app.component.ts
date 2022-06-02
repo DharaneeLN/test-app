6c4241a8cfa9496fc5b0a8eae7df3862c03dd28b
Dharanee <dharanee@nfactorialanalytics.com>
2022-06-02 13:12:03 +0530

    Added web worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to my test application';
+
+  // tslint:disable-next-line: typedef
+  runWorker() {
+
+    const testWebWorker = new Worker('./app.worker', { type: 'module'});
+
+    testWebWorker.onmessage = (message) => {
+      console.log(`Got message`, message.data);
+    };
+
+    testWebWorker.postMessage('hello');
+
+  }
 }
